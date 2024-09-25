fb1b54231a6977f57e756eea5f04f0037aa79fa1
tuan <minhtuankf@gmail.com>
2024-09-25 11:01:00 +0700

    add ui

---
+++ ARS_FE/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ARS_FE.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
