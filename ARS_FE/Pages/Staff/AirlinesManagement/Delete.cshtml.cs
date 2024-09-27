f5de80f7258cc5aae44a73a8800b2593e7097fcd
PhuHoang07 <duongphu1207@gmail.com>
2024-09-27 10:36:24 +0700

    [feat]: + layoutmanage + API Helper + email service [fix]: + new DB scripts + scaffold again

---
+++ ARS_FE/Pages/Staff/AirlinesManagement/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using DAO;
+
+namespace ARS_FE.Pages.Staff.AirlinesManagement
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DAO.AirlinesReservationSystemContext _context;
+
+        public DeleteModel(DAO.AirlinesReservationSystemContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Airline Airline { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airline = await _context.Airlines.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (airline == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Airline = airline;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airline = await _context.Airlines.FindAsync(id);
+            if (airline != null)
+            {
+                Airline = airline;
+                _context.Airlines.Remove(Airline);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ARS_FE/Pages/Staff/AirlinesManagement/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using DAO;
+using Service.Services.AIrlineServices;
+using Newtonsoft.Json;
+
+namespace ARS_FE.Pages.Staff.AirlinesManagement
+{
+    public class IndexModel : PageModel
+    {
+
+        public IndexModel()
+        {
+        }
+
+        public IList<Airline> Airline { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await APIHelper.GetAsJsonAsync<List<Airline>>(httpClient, APIHelper.Url + "airline");
+                if (response != null)
+                {
+                    Airline = response;
+                }
+                else
+                {
+                    Airline = new List<Airline>();
+                }
+            }
+        }
+    }
+}
