4022cc356e3b19dcbf665309142c5e4197bd6d36
An Xemer <anxemer224@gmail.com>
2024-10-10 19:39:11 +0700

    fix UI

--- BusinessObjects/RequestModels/Airplane/UpdateAirplaneRequest.cs
+++ BusinessObjects/RequestModels/Airplane/UpdateAirplaneRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,19 @@ namespace BusinessObjects.RequestModels.Airplane
     {
         public string Id { get; set; } = null!;
 
+        [Required(ErrorMessage = "Code is required.")]
+        [StringLength(10, ErrorMessage = "Code cannot be longer than 10 characters.")]
         public string Code { get; set; } = null!;
 
+        [Required(ErrorMessage = "Type is required.")]
         public string Type { get; set; } = null!;
 
+        [Range(1, int.MaxValue, ErrorMessage = "AvailableSeat must be greater than 0.")]
         public int AvailableSeat { get; set; }
 
         public string? AirlinesId { get; set; }
 
+        [RegularExpression("^(Active|Inactive)$", ErrorMessage = "Status must be either 'Active' or 'Inactive'.")]
         public string? Status { get; set; }
     }
 }
